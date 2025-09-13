2b182ebad6527fbf093b779e08a9cfa9c91c15c6
Loc Giang <locbgiang@gmail.com>
2025-09-13 13:38:22 -0700

    MerkleAirdrop.sol

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -2,21 +2,44 @@
 pragma solidity ^0.8.24;
 
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {SignatureChecker} from "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
 import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract MerkleAirdrop is EIP712 {
+    // library attachement 
+    // adds ECDSA cryptographic functions directly to the bytes32 type
+    using ECDSA for bytes32;
+    using SafeERC20 for IERC20;
+
     error MerkleAirdrop__AlreadyClaimed();
     error MerkelAirdrop__InvalidSignature();
+    error MerkleAirdrop__InvalidProof();
 
+    // the token that will be use for airdropping
+    // data from constructor
+    IERC20 private immutable i_airdropToken;
+
+    // i_merkleRoot is a hash that has the property to prove whether any other hash belongs to its tree
+    // like having a compressed list that can verify membership without storing the actual list
+    // mapping(address => uint256) allowedClaims; would be expensive 
+    // data from constructor
+    bytes32 private immutable i_merkleRoot;
+
+    // to keep track of who has claimed the airdrop already
     mapping(address => bool) s_hasClaimed;
 
     // this is a unique identifier for EIP-712 
     // this ensures that the signatures are tied to a specific data structure format
     bytes32 private constant MESSAGE_TYPEHASH = keccak256("AirdropClaim(address account, uint256 amount)");
 
-    constructor() EIP712("Merkle Airdrop", "1.0.0") {}
+    event Claimed(address account, uint256 amount);
+
+    constructor(bytes32 merkleRoot, IERC20 airdropToken) EIP712("Merkle Airdrop", "1.0.0") {
+        i_merkleRoot = merkleRoot;
+        i_airdropToken = airdropToken;
+    }
 
     struct AirdropClaim {
         address account;
@@ -25,6 +48,9 @@ contract MerkleAirdrop is EIP712 {
 
     /**
      * This function allows a user to claim their airdrop allocation.
+     * Several checks are in place to make sure there are no
+     * 1. double claiming
+     * 2. 
      * @param account account of the claimer
      * @param amount amount to claim
      * @param merkleProof the merkle proof to verify the claim
@@ -45,16 +71,18 @@ contract MerkleAirdrop is EIP712 {
             revert MerkelAirdrop__InvalidSignature();
         }
 
-        // verify merkle proof
+        // verify account/amount is part of the merkle tree
         bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
         if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
-
+            revert MerkleAirdrop__InvalidProof();
         }
 
         // mark as claimed
         s_hasClaimed[account] = true;
+        emit Claimed(account, amount);
 
         // transfer the token
+        i_airdropToken.safeTransfer(account, amount);
     }
 
     /**
@@ -71,7 +99,11 @@ contract MerkleAirdrop is EIP712 {
         // _hashTypedDataV4 is a function from OpenZeppelin's EIP-712 contract
         // it creates the final hash used for EIP-712 signature verification
         return _hashTypedDataV4(
+            // hashes the encoded bytes into a fixed 32-byte hash
+            // creates a deterministic output (same input = same hash)
             keccak256(
+                // abi.encode converts data into bytes
+                // packs multiple values into a single byte array
                 abi.encode(
                     MESSAGE_TYPEHASH,       // tye indetifier, prevent signature replay attacks
                     AirdropClaim({          // the actual data
@@ -82,6 +114,18 @@ contract MerkleAirdrop is EIP712 {
             )
         );
     } 
+
+    /////////////////////////////////////////////////////////
+    // View and Pure ////////////////////////////////////////
+    /////////////////////////////////////////////////////////
+
+    function getMerkleRoot () external view returns(bytes32) {
+        return i_merkleRoot;
+    }
+
+    function getAirdropTOken() external view returns(IERC20) {
+        return i_airdropToken;
+    }
     
     
     ///////////////////////////////////////////////////////////////////
